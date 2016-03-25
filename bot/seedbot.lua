d576d0fa8ecc9e5bd0eb6c62c4baecc50dacffcf
avast team! <titanteam0098@gmail.com>
2016-03-25 17:35:56 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,20 +226,25 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {110626080,103649648,143723991,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {164985762},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
 An advance Administration bot based on yagop/telegram-bot 
 
-https://github.com/SEEDTEAM/TeleSeed
+https://github.com/Arashavast/avast-finver-bot/
 
 Our team!
-Alphonse (@Iwals)
-I M /-\ N (@Imandaneshi)
-Siyanew (@Siyanew)
-Rondoozle (@Potus)
-Seyedan (@Seyedan25)
+Arash avast ( @Arash_avast )
+Hacker mohammad angel ( @in_acc_mord )
+Arman ( @paydar_021 )
+BAD BOY redteam ( @redteam_02_19 )
+Arian Triex ( @ArianTriex )
+Mersad nph ( @Mersad_nph )
+Alireza avast ( @Alireza_avast )
+Hossein Treix ( @HosseinTriex )
+Mohammad core ( @mohammad20162015 )
+
 
 Special thanks to:
 Juan Potato
